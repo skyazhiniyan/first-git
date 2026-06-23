4f718a069cd34c8bffb6e105e3f0e15f5c1c2a0e
yazhiniyan <s.k.yazhiniyan@gmail.com>
2026-06-23 11:51:09 +0530

    first commit

--- calculator.py
+++ calculator.py
@@ -1,2 +1,13 @@
-print("addition",2+5)
-print("subraction",2-5)
+a=int(input("Enter any number: "))
+b=int(input("Enter any number: "))
+c=int(input("Enter any symbol: "))
+if(c=="+"):
+    print(a+b)
+if(c=="-"):
+    print(a-b)
+if(c=="*"):
+    print(a*b)
+if(c=="/"):
+    print(a/b)
+else:
+    print("Invalid symbol")
---
+++ super _calculator.py
@@ -0,0 +1,13 @@
+a=int(input("Enter any number:2 "))
+b=int(input("Enter any number:2 "))
+c=int(input("Enter any symbol: "))
+if(c=="+"):
+    print(a+b)
+elif c =="-":
+    print(a-b)
+elif(c=="*"):
+    print(a*b)
+elif(c=="/"):
+    print(a/b)
+else:
+    print("Invalid symbol")
